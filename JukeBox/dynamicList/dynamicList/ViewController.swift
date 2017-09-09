656ccdda73be6647dfa1b33689161c25f924f089
rcohen22345 <rsc30@duke.edu>
2017-09-09 17:58:37 -0400

    initial commit

---
+++ JukeBox/dynamicList/dynamicList/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  dynamicList
+//
+//  Created by R C on 2017-09-09.
+//  Copyright © 2017 JukeBox. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
